19f81ffd59b160abb5dcd47be2c283759c5a3d9c
matildanegoita <negoita.cl.ioana@student.utcluj.ro>
2025-01-23 11:37:44 +0200

    added pages for menu

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { MenuComponent } from './app/components/menu/menu.component';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(MenuComponent, {
-  providers: [importProvidersFrom(HttpClientModule)],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
